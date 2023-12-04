8880652293cdfe87408f67a6c77b3c95b5a475de
Laukkanen <mattilau@ad.helsinki.fi>
2023-12-04 15:14:57 +0200

    exc. 5

--- data/Create_human.R
+++ data/Create_human.R
@@ -26,3 +26,38 @@ library(readr)
 human <- inner_join(gii, hd, by = "Country")
 
 write_csv(human, "data/human.csv")
+
+#continuing from human data:
+
+library(readr)
+human <- read_csv("data/human.csv")
+str(human)
+dim(human)
+#our dataset consist of 195 observations of 19 variables. In the dataset, we examine the Human Development Index (HDI) and the factors affecting it. HDI is a index developed by UN to assess the development of a country, instead of looking just the GDP growth.
+# In  the dataset, we compare countries with respect to their HDI and HDI rank, Global Innovation Index (GII) and GII rank, maternal mortality, adolescent birth rate, Education, labor, life and education expectancy, as well as their Gross National Income (GNI)
+
+#We will eliminate all variables that are not necessary for our purposes, and leave only the following nine variables:  "Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F"
+
+
+Human_new <- human[, c("Country", "Edu2.ratio", "Labo.ratio", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli")]
+
+
+#Next, I want to eliminate all rows with missing values:
+
+Human_new <- na.omit(Human_new)
+
+#Next, I want to eliminate all observations that are regions instead of countries. Seems like all observations 156-162 are regions. Let us eliminate those:
+
+Human_new <- Human_new[-c(156:162), ]
+
+#Let us save the data in our folder and ovewrite the old dataset:
+
+write_csv(Human_new, "data/human.csv")
+
+#check if everything is in order:
+
+library(readr)
+human <- read_csv("data/human.csv")
+
+#bingo
+
